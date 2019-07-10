11f141487bca1ebcce443ae5423d190e9c2f67a7
Nancy T <tornan@gmail.com>
2019-07-10 10:02:19 -0500

    Add files via upload

--- logic.js
+++ logic.js
@@ -12,70 +12,41 @@ L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     accessToken: API_KEY
   }).addTo(myMap);
   
- //------------------------------------------------- 
-
 // API URL
 var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson"
-console.log(url);
+
+// Fetch data and create a dictionary
+var dict = [];
+var tmp = {};
 
 d3.json(url,function(response) {
-    console.log(response);
-//});
+//  console.log(response);
+//  console.log(response.features.length);
+  for (var i = 0; i < response.features.length; i++) {
+  var title = response.metadata.title
+  var lat = response.features[i].geometry.coordinates[1]
+  var lon = response.features[i].geometry.coordinates[0]
+  
+  tmp['title'] = title;
+  tmp['lat'] = lat;
+  tmp['lon'] = lon;
 
-//Primer elemento del Json
-console.log("response.metadata");
- console.log(response.metadata);
- console.log("response.metadata.count");
- console.log(response.metadata.count);
+  dict.push(tmp);
+  console.log(tmp);
+  console.log(dict);
+  console.log(JSON.stringify(dict));
 
-console.log("response.features");
+// Create a new marker cluster group
+var markers = L.markerClusterGroup();
 
-console.log(response.features);
+// Add a new marker to the cluster group and bind a pop-up
+markers.addLayer(L.marker([response.features[i].geometry.coordinates[1],response.features[i].geometry.coordinates[0] ])
+.bindPopup(response.metadata.title));   
 
-console.log("response.features.length");
-console.log(response.features.length);
-featuresData = response.features
- for (var i = 0; i < featuresData.length; i++) {
-      var feature =featuresData[i];
-      console.log("feature.id");
-      console.log(feature.id);
-      console.log("feature.geometry.coordinates[0]");
-      for (var j = 0; j < feature.geometry.coordinates.length; j++) {
-        console.log(j);
-        console.log(feature.geometry.coordinates[j]);
-      }
-      console.log(feature.geometry.coordinates[0]);
-  }
+// Add our marker cluster layer to the map
+myMap.addLayer(markers)
+
+}
 });
 
 
-
-
-// // Grab the data with d3
-// d3.json(url, function(response) {
-
-//   // Create a new marker cluster group
-//   var markers = L.markerClusterGroup();
-
-//   // Loop through data
-//   for (var i = 0; i < response.length; i++) {
-
-//     // Set the data location property to a variable
-//     //var location = response[i].location;
-//     var type = response[i].type.geometry;
-    
-//     // Check for location property
-//     if (type) {
-
-//       // Add a new marker to the cluster group and bind a pop-up
-//       markers.addLayer(L.marker([type.coordinates[1], type.coordinates[0]])
-//         .bindPopup(response[i].descriptor));
-//         console.log(response[i].descriptor);
-//     }
-
-//   }
-
-//   // Add our marker cluster layer to the map
-//   myMap.addLayer(markers)
-
-// });
